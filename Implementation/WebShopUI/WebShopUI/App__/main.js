4b18a9006fb9566898e423aeef04d45f9d0983cf
Lovas Istvan <l.pista@hotmail.com>
2014-03-05 07:18:01 +0100

    VS Project, Durandal, Bootstrap bekerült; index.html kész, shell.html elkezd (menűk, keretek)

---
+++ Implementation/WebShopUI/WebShopUI/App__/main.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../Scripts/durandal/viewLocator.js" />
+requirejs.config({
+    path: {
+        'text': '../Scripts/text.js'
+    }
+});
+
+define(function (require) {
+    var app = require('../Scripts/durandal/viewLocator.js'),
+        viewLocator
+
+});
